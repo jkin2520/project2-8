8a90f64c28190efc404717565f6d011651b87480
Jalen King <jkin2520@students.vsu.edu>
2024-12-06 06:23:40 -0500

    Added documentation for getCurrentDateTime function

---
+++ TimeFunctions.java
@@ -0,0 +1,13 @@
+import java.time.*;
+/**
+ * Utility class for time-related functions.
+ */
+public class TimeFunctions {
+    /**
+     * Returns the current date and time.
+     * @return LocalDateTime object of the current date and time.
+     */
+    public static LocalDateTime getCurrentDateTime() {
+        return LocalDateTime.now();
+    }
+}
